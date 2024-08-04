06a4be3e19218ab7bc9b1219f8a9ec14ab85abf9
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2024-08-04 18:36:59 +0200

    build(renderer): Add library metadata

--- renderer/build.gradle.kts
+++ renderer/build.gradle.kts
@@ -15,3 +15,14 @@ dependencies {
 	compileOnly(libs.dokka.base)
 	compileOnly(libs.dokka.core)
 }
+
+library {
+	name.set("Dokka render for Material for MkDocs")
+	description.set("Dokka plugin that adds the Material for MkDocs format")
+	homeUrl.set("https://gitlab.com/opensavvy/automation/dokka-material-mkdocs")
+
+	license.set {
+		name.set("Apache 2.0")
+		url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+	}
+}
