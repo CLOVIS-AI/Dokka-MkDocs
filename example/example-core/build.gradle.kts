df0a6a36d1e067600706920f2348bbcb5fbb8a8a
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2024-08-06 20:59:10 +0200

    test(example): Hack to use the workaround

--- example/example-core/build.gradle.kts
+++ example/example-core/build.gradle.kts
@@ -10,3 +10,11 @@ kotlin {
 		nodejs()
 	}
 }
+
+// Workaround to use the module from this repository instead of using the one that is
+// published. In a real project using the plugin, do not copy this!
+configurations.all {
+	resolutionStrategy.dependencySubstitution {
+		substitute(module("dev.opensavvy.dokka.mkdocs:renderer")).using(project(":renderer"))
+	}
+}
