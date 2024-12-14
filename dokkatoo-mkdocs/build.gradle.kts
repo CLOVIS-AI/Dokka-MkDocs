4a826f1976c17454bf115236507d50d5f23d6d0d
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2024-12-14 20:10:13 +0100

    fix(dokkatoo-mkdocs): Declare the current version of the renderer as dependency

--- dokkatoo-mkdocs/build.gradle.kts
+++ dokkatoo-mkdocs/build.gradle.kts
@@ -1,3 +1,6 @@
+import dev.adamko.dokkatoo.tasks.DokkatooGenerateTask
+import org.gradle.kotlin.dsl.withType
+
 plugins {
 	`kotlin-dsl`
 	alias(opensavvyConventions.plugins.base)
@@ -27,4 +30,36 @@ library {
 		name.set("Apache 2.0")
 		url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
 	}
+}
+
+val embedCurrentVersion by tasks.registering {
+	description = "Embeds the current version into the source code"
+
+	val version = project.version as String
+	val file = File(kotlin.sourceSets.main.get().kotlin.srcDirs.first(), "version.kt")
+
+	inputs.property("version", version)
+	outputs.file(file)
+
+	doLast {
+		file.writeText(
+			"""
+				// Generated file, do not edit
+				package opensavvy.dokka.gradle
+				internal const val DokkaMkDocsVersion = "$version"
+			""".trimIndent()
+		)
+	}
+}
+
+tasks.compileKotlin {
+	dependsOn(embedCurrentVersion)
+}
+
+tasks.sourcesJar {
+	dependsOn(embedCurrentVersion)
+}
+
+tasks.withType<DokkatooGenerateTask> {
+	dependsOn(embedCurrentVersion)
 }
