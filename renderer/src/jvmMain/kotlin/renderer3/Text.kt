f3ff4b4a3036bb8199d686ce1568c3ce50a08008
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2026-04-16 19:46:42 +0200

    fix(renderer): Fix the rendering of annotations
    Before:
    ```kotlin
    @JvmName
    (name = "foo_int")fun foo(number: Int): Boolean
    ```
    
    After:
    ```kotlin
    @JvmName(name = "foo_int")
    fun foo(number: Int): Boolean
    ```

--- renderer/src/jvmMain/kotlin/renderer3/Text.kt
+++ renderer/src/jvmMain/kotlin/renderer3/Text.kt
@@ -38,10 +38,6 @@ internal fun RenderingContext.buildText(node: ContentText) {
 	decorateWith(textStyles.matches(node)) {
 		append(node.text)
 	}
-
-	if (node.hasStyle(TokenStyle.Annotation) && node.text != "@") {
-		appendLine()
-	}
 }
 
 internal fun RenderingContext.buildCodeInline(node: ContentCodeInline) {
